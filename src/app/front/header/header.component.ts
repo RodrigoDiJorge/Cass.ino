716c32cead38ee6b375618e305544340da633d4c
Marco Patrocinio <marcopatrocinio@ucl.br>
2020-08-14 06:14:25 -0300

    header ta ficano bunito kkkk

--- src/app/front/header/header.component.ts
+++ src/app/front/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
+  Jogos;
+  Regras;
 
   ngOnInit(): void {
   }
-
+  ngAfterViewInit(){
+    this.Jogos = document.getElementById('hcbn-Jogos');
+    this.Regras = document.getElementById('hcbn-Regras');
+  }
+  showNav(ind)
+  {
+    switch(ind)
+    {
+      case 1:
+        this.Jogos.style.display = "block";
+        break;
+      case 2:
+        this.Regras.style.display = "block";
+        break;
+    }
+  }
+  hideNav(ind)
+  {
+    let elementos = document.getElementById("hcbn-Jogos");
+    switch(ind)
+    {
+      case 1:
+        this.Jogos.style.display = "none";
+        break;
+      case 2:
+        this.Regras.style.display = "none";
+        break;
+    }
+  }
 }
