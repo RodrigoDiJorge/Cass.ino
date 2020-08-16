5fd3037f07b26aadd5c4018917a2ceec936326dd
Marco Patrocinio <marcopatrocinio@ucl.br>
2020-08-16 20:06:37 -0300

    basico da tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './front/home/home.component';
 import { CadastrarComponent } from './front/cadastrar/cadastrar.component';
+import { LoginComponent } from './front/login/login.component';
 
 const routes: Routes = [
   {
     path: "home",
     component:HomeComponent
+  },
+  {
+    path:"login",
+    component:LoginComponent
   },
   {
     path: "cadastrar",
