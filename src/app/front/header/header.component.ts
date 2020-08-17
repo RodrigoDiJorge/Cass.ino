f20e320cc00fa86ae91db9810469d99588859473
Marco Patrocinio <marcopatrocinio@ucl.br>
2020-08-16 21:36:55 -0300

    animação no dropdown

--- src/app/front/header/header.component.ts
+++ src/app/front/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, ElementRef} from '@angular/core';
 import { Router } from '@angular/router';
+import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
 
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }]
 })
 export class HeaderComponent implements OnInit {
 
